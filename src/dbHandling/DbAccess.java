15b8b6da3e0cb263972b12874e6b06a272a123aa
warrantEagle <aro_1595@wp.pl>
2018-05-26 16:05:07 +0200

    Database access.

--- src/dbHandling/DbAccess.java
+++ src/dbHandling/DbAccess.java
@@ -1,15 +1,23 @@
 package dbHandling;
 
-public class DbAccess {
-    IObjectContainer db =
-            Db4oEmbedded.OpenFile(Db4oEmbedded.NewConfiguration(), YapFileName);
-try
-    {
-        // do something with db4o
-    }
-finally
-    {
-        db.Close();
-    }
+import com.db4o.Db4o;
+import com.db4o.ObjectContainer;
+import person.Student;
 
+public class DbAccess {
+
+    public DbAccess(){
+
+
+    ObjectContainer db=Db4o.openFile("dbo4File");
+
+    try {
+            // do something with db4o
+            Student student1=new Student("Michael Schumacher");
+            db.store(student1);
+        }
+    finally {
+            db.close();
+        }
+    }
 }
